56c6c85d1f848499864868a247ca464ac54a4c14
Salih Yürek <44748437+salihyurek@users.noreply.github.com>
2025-04-11 19:05:51 +0300

    Update Form1.Designer.cs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///Gerekli tasarımcı değişkeni.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///Kullanılan tüm kaynakları temizleyin.
-        /// </summary>
-        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +16,7 @@
 
         #region Windows Form Designer üretilen kod
 
-        /// <summary>
-        /// Tasarımcı desteği için gerekli metot - bu metodun 
-        ///içeriğini kod düzenleyici ile değiştirmeyin.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             this.SuspendLayout();
