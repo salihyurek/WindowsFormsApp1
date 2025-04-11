bc2853581cfe9dc4a4414c46e3dd975832b3fec2
salih <salih@WIN-J871JR05NEG>
2025-04-11 18:59:10 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ColorChangerForm
+{
+    public partial class Form1 : Form
+    {
+        private List<Color> renkListesi;
+        private int renkIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 10 farklı rengi tanımlıyoruz
+            renkListesi = new List<Color>
+            {
+                Color.Red,
+                Color.Blue,
+                Color.Green,
+                Color.Yellow,
+                Color.Orange,
+                Color.Purple,
+                Color.Pink,
+                Color.Teal,
+                Color.Brown,
+                Color.Gray
+            };
+
+            // İlk rengi uygula
+            this.BackColor = renkListesi[renkIndex];
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // Enter tuşuna basıldığında renk değiştir
+            if (keyData == Keys.Enter)
+            {
+                renkIndex = (renkIndex + 1) % renkListesi.Count; // Liste sonuna geldiğinde başa dön
+                this.BackColor = renkListesi[renkIndex];
+                return true; // İşlendiğini belirt
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
+
